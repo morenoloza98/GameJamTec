ca7406932eb0c475ab0c195f5d738ff94a731f4d
morenoloza98 <morenolozalex98@gmail.com>
2019-11-07 20:53:42 -0600

    Backgroudn and camera

---
+++ CookieRunner/Assets/Scripts/FullscreenImage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullscreenImage : MonoBehaviour
+{
+    void Awake()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        Vector3 cameraSize = new Vector3(Camera.main.aspect * cameraHeight, cameraHeight, 40);
+        Vector3 spriteSize = spriteRenderer.sprite.bounds.size;
+
+        Vector3 scale = transform.localScale;
+        if (cameraSize.x >= cameraSize.y)
+        { // Landscape (or equal)
+            scale *= cameraSize.x / spriteSize.x;
+        }
+        else
+        { // Portrait
+            scale *= cameraSize.y / spriteSize.y;
+        }
+
+        //transform.position = Vector3.zero; // Optional
+        transform.localScale = scale;
+    }
+}
